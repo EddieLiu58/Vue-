16cbcffc323feca49fb0ffd0560fac07a05440e3
EddieLiu58 <45484194+EddieLiu58@users.noreply.github.com>
2018-12-24 13:21:06 +0800

    Update

---
+++ list.js
@@ -0,0 +1,14 @@
+let data = {
+    name: 'Eddie',
+    sex:'男',
+    age:'24歲',
+    favor:['code','攝影','健身','唱歌'],
+    member:true
+}
+
+let vm = new Vue ({
+    el: '#app',
+    data:data
+
+
+})
